f48ae322f140d7a22c4db762697f6b236be9f309
wenbin <wenbin.shi@gmail.com>
2022-08-01 16:31:30 +0800

    add 1_deployer.sh

---
+++ SplitDemo/1_deployer.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+
+function buildDeployer(){
+  cp -r docker/deployer/Dockerfile optimism/Dockerfile
+  cd optimism
+  docker build -t davionlabs/deployer .
+
+  #clean the Dockerfile
+  rm -rf Dockerfile
+  cd ..
+}
+
+
+function startDeployer(){
+  docker run --net bridge -itd   -p 8080:8081 -e "AUTOMATICALLY_TRANSFER_OWNERSHIP=true" \
+  -e "ETHERSCAN_API_KEY=B1XAN986315AME96W9QK7X1RGQ6WJMWEPW" \
+  -e "CONTRACTS_RPC_URL=https://rinkeby.infura.io/v3/d2e240ec3a474c6b8e7599eabce9fbae" \
+  -e "CONTRACTS_DEPLOYER_KEY=6395A7C842A08515961888D21D72F409B61FBCE96AF1E520384E375F301A8297" \
+  -e "CONTRACTS_TARGET_NETWORK=rinkeby" --entrypoint "/opt/optimism/packages/contracts/deployer.sh" \
+   --name=deployer davionlabs/deployer
+}
+
+buildDeployer
+startDeployer
