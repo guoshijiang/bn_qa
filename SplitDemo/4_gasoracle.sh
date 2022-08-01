ab1b835d55fed992f757b316f46c2aecde966360
wenbin <wenbin.shi@gmail.com>
2022-08-01 17:51:27 +0800

    update oracle private key

--- SplitDemo/4_gasoracle.sh
+++ SplitDemo/4_gasoracle.sh
@@ -12,7 +12,7 @@ function startGasOracle(){
   docker run --net bridge -itd --restart unless-stopped \
   -e "GAS_PRICE_ORACLE_ETHEREUM_HTTP_URL=https://rinkeby.infura.io/v3/d2e240ec3a474c6b8e7599eabce9fbae" \
   -e "GAS_PRICE_ORACLE_LAYER_TWO_HTTP_URL=http://172.17.0.1:8545" \
-  -e "GAS_PRICE_ORACLE_PRIVATE_KEY=0x8b3a350cf5c34c9194ca85829a2df0ec3153be0318b5e2d3348e872092edffba" \
+  -e "GAS_PRICE_ORACLE_PRIVATE_KEY=0x6395A7C842A08515961888D21D72F409B61FBCE96AF1E520384E375F301A8297" \
   --name=gas-oracle davionlabs/gas-oracle
 }
 
