6be86423e5700abe3136a49d7d4630c779ff5421
wenbin <wenbin.shi@gmail.com>
2022-08-01 16:48:32 +0800

    add qa

---
+++ README.md
@@ -0,0 +1,3 @@
+# bn_qa
+
+This is the scripts for qa env of bn.
---
+++ SplitDemo/2_l2geth.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+
+function  buildL2Geth() {
+  # build l2-geth
+  cd optimism/
+  docker build -f l2geth/Dockerfile -t davionlabs/l2geth .
+  cd ../
+}
+
+function replaceEnv(){
+  cp -r envs/geth.template.env envs/geth.env
+}
+
+function startL2(){
+  docker run --net bridge -itd -p 8545:8545 -p 8546:8546 --env-file envs/geth.env --restart unless-stopped --entrypoint "/usr/local/bin/geth.sh" --name=l2_geth davionlabs/l2geth
+}
+
+
+buildL2Geth
+replaceEnv
+startL2
