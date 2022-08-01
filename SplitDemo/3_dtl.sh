71f36285b4de847568e57fce22c6cf3145c22b5e
wenbin <wenbin.shi@gmail.com>
2022-08-01 16:52:42 +0800

    add dtl

---
+++ SplitDemo/3_dtl.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+
+function buildDtl(){
+  cp -r docker/dtl/Dockerfile optimism/Dockerfile
+  cd optimism
+  docker build -t davionlabs/data-transport-layer .
+
+  #clean the Dockerfile
+  rm -rf Dockerfile
+  cd ..
+}
+
+
+function replaceEnv(){
+  cp -r envs/dtl.template.env envs/dtl.env
+}
+
+function startDtl(){
+  docker run --net bridge -itd -p 7878:7878 --env-file envs/dtl.env \
+  --restart unless-stopped --entrypoint "/opt/optimism/packages/data-transport-layer/dtl.sh" --name=dtl davionlabs/data-transport-layer
+}
+
+buildDtl
+replaceEnv
+startDtl
