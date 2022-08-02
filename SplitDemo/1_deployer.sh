298b4c11bd91adfd34ad32969dcfd2bc4f48a809
wenbin <wenbin.shi@gmail.com>
2022-08-02 20:45:05 +0800

    add reusing

--- SplitDemo/1_deployer.sh
+++ SplitDemo/1_deployer.sh
@@ -12,7 +12,7 @@ function buildDeployer(){
 
 
 function startDeployer(){
-  docker run --net bridge -itd   -p 8080:8081 -e "AUTOMATICALLY_TRANSFER_OWNERSHIP=true" \
+  docker run --net bridge -itd  --restart unless-stopped  -p 8080:8081 -e "AUTOMATICALLY_TRANSFER_OWNERSHIP=true" \
   -e "ETHERSCAN_API_KEY=B1XAN986315AME96W9QK7X1RGQ6WJMWEPW" \
   -e "CONTRACTS_RPC_URL=https://ropsten.infura.io/v3/d2e240ec3a474c6b8e7599eabce9fbae" \
   -e "CONTRACTS_DEPLOYER_KEY=6395A7C842A08515961888D21D72F409B61FBCE96AF1E520384E375F301A8297" \
